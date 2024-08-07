9f74d0bdd898b042cedfe5c02a12fca105e5c297
Jake Lim <jakelim070@gmail.com>
2024-08-07 15:57:37 +0800

    cleanup

--- activate.ps1
+++ activate.ps1
@@ -1,16 +1,18 @@
-param([string]$pythonEnvName="venv")
-$activateScriptName = "Activate.ps1"
-$isValid = $true
-Write-Host "activating python=$pythonEnvName..."
-# $scriptPath = Join-Path -Path $env:USERPROFILE "venv" "Scripts" $activateScriptName # Works only on powershell7
-$scriptPath = Join-Path $env:USERPROFILE -ChildPath $pythonEnvName | Join-Path -ChildPath "Scripts" | Join-Path -ChildPath $activateScriptName    
 
-if (Test-Path -Path $scriptPath) 
-{
-    & $scriptPath
-}
-else
-{
-    throw "missing dir (python=$pythonEnvName)..."
+function activatepy {
+    
+    param([string]$pythonEnvName = "venv")
+    $activateScriptName = "Activate.ps1"
+    Write-Host "activating python=$pythonEnvName..."
+    $scriptPath = Join-Path $env:USERPROFILE -ChildPath $pythonEnvName | Join-Path -ChildPath "Scripts" | Join-Path -ChildPath $activateScriptName    
+    
+    if (Test-Path -Path $scriptPath) {
+        & $scriptPath
+    }
+    else {
+        throw "missing dir (python=$pythonEnvName)..."
+    }
+    
+    
 }
 
